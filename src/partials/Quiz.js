552cf6eaecd623ec10ae486d2b2bbc57d74ae82d
Henrik Nygren <nygrenh@gmail.com>
2020-01-30 08:20:47 +0200

    Quiz maintenance

--- src/partials/Quiz.js
+++ src/partials/Quiz.js
@@ -1,13 +1,9 @@
 import React from "react"
 import styled from "styled-components"
 import LoginStateContext from "../contexes/LoginStateContext"
-import LoginControls from "../components/LoginControls"
 
 import withSimpleErrorBoundary from "../util/withSimpleErrorBoundary"
-import { normalizeExerciseId } from "../util/strings"
-import Quiz from "moocfi-quizzes"
 import { Paper } from "@material-ui/core"
-import { accessToken } from "../services/moocfi"
 
 const StyledPaper = styled(Paper)`
   overflow: hidden;
@@ -20,41 +16,13 @@ class QuizPartial extends React.Component {
   static contextType = LoginStateContext
 
   render() {
-    if (!this.context.loggedIn) {
-      const loginPrompt = (
-        <div style={{ padding: "1rem", textAlign: "center" }}>
-          <p>Kirjaudu sisään nähdäksesi tehtävän.</p>
-          <LoginControls />
-        </div>
-      )
-
-      return (
-        <StyledPaper id={normalizeExerciseId(`quiz-${id}`)}>
-          <Quiz
-            id={id}
-            languageId="fi_FI"
-            backendAddress="https://quizzes.mooc.fi"
-            customContent={loginPrompt}
-            showAlwaysPointsInfo={false}
-          />
-        </StyledPaper>
-      )
-    }
-
-    const { id } = this.props
-    if (!id) {
-      return <div>There should be quiz here but no quiz id is specified.</div>
-    }
     return (
-      <StyledPaper id={normalizeExerciseId(`quiz-${id}`)}>
-        <Quiz
-          id={id}
-          languageId="fi_FI"
-          accessToken={accessToken()}
-          backendAddress="https://quizzes.mooc.fi"
-          showAlwaysPointsInfo={false}
-        />
-      </StyledPaper>
+      <div>
+        <b>
+          Tässä kohtaa oleva kysely on huoltokatkolla. Kysely palaa katkolta
+          perjantaina 31.1.2020.
+        </b>
+      </div>
     )
   }
 }
