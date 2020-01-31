7df8554448056c38bd72ab81aaffcb96d412a314
Henrik Nygren <nygrenh@gmail.com>
2020-01-31 17:14:31 +0200

    Revert "Fix typo"
    This reverts commit 1ee371b87a8ee2312cba41c73b3307c559eb6dec.

--- src/partials/Quiz.js
+++ src/partials/Quiz.js
@@ -17,7 +17,7 @@ class QuizPartial extends React.Component {
     return (
       <StyledCard>
         <b>
-          Tässä kohtaa oleva kurssin tehtävä on huoltokatkolla. Tehtävä palaa
+          Tässä kohtaa oleva kurssin tehtävä on huoltokatkolla. Tehävä palaa
           katkolta perjantaina 31.1.2020.
         </b>
       </StyledCard>
